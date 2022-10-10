1f6398f6b09dbba924e9a0454fd225e4c9623828
Oleh Dziuba <oleg_dzuba@outlook.com>
2022-10-10 19:22:49 +0300

    add comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,28 @@
 #include "calculator.h"
 
+/// @brief  
+/// @param a - first num
+/// @param b - second num
+/// @return a + b + 0.5
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+/// @brief
+/// @param a first number 
+/// @param b second number
+/// @return a - b
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+
+/// @brief  
+/// @param a - first num
+/// @param b - second num
+/// @return a * b + 0.5
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
